20092853f55f1e0700e72d713aa9fa68139ba316
Anna <michalowska.aneczka@gmail.com>
2022-11-02 19:26:20 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryFirst = document.querySelector(".gallery");
+
+const instance = basicLightbox.create(`
+   <img src="" width="800" height="600">
+`);
+
+instance.show();
 
 console.log(galleryItems);
