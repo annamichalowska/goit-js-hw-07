9fd9e8a8854e313cb03fbaa6e7e9d7968446e90b
Anna <michalowska.aneczka@gmail.com>
2022-11-03 19:24:12 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,37 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryFirst = document.querySelector(".gallery");
+const placeOfGallery = document.querySelector(".gallery");
+const mainGallery = galleryItems;
 
-const instance = basicLightbox.create(`
-   <img src="" width="800" height="600">
-`);
+const createGallery = mainGallery
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
 
-instance.show();
+placeOfGallery.insertAdjacentHTML("beforeend", createGallery);
 
-console.log(galleryItems);
+placeOfGallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bigImg = event.target.dataset.source;
+  if (!bigImg) return;
+
+  const instance = basicLightbox.create(`
+<img src="${bigImg}" width="800" height="600"></img>
+ `);
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && instance.visible()) instance.close();
+  });
+});
